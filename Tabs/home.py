4b15aa1f46f4d63ae41114e198700343bec6a971
SRI YUVA RAANI S P <108577818+Itsyuvaa@users.noreply.github.com>
2023-10-11 22:16:11 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -5,9 +5,6 @@ import streamlit as st
 
 def app():
     """This function create the home page"""
-    
-    # Add title to the home page
-    st.title("Stress Level Detector")
 
     # Add image to the home page
     st.image("./images/home.png")
